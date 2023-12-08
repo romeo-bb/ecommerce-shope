7e2370cf8a84d2a7df2657c014e880c497e81ac8
mohammed <mandalosy13@gmail.com>
2023-12-08 12:33:05 +0100

    finished project

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'udkzfsd4',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/index.ts
@@ -0,0 +1,5 @@
+import Category from "./Category";
+import Product from "./Product";
+import heroimages from "./heroimages";
+
+export const schemaTypes = [Product, Category, heroimages]
