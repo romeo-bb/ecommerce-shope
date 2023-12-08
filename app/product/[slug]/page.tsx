bc6b1a40e9d3c3c436b04ac331df9ec9c9b9fa8e
mohammed <mandalosy13@gmail.com>
2023-12-08 13:12:19 +0100

    fixed 2 prb

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import AddToBag from "@/app/components/AddToBag";
-// import CheckoutNow from "@/app/components/CheckoutNow";
+import CheckoutNow from "@/app/components/CheckoutNow";
 import ImageGallery from "@/app/components/ImageGallery";
 import { fullProduct } from "@/app/interface";
 import { client } from "@/app/lib/sanity";
@@ -89,7 +89,7 @@ export default async function ProductPge({
                 key={data._id}
                 price_id={data.price_id}
               />
-              {/* <CheckoutNow
+              <CheckoutNow
                 currency="USD"
                 description={data.description}
                 image={data.images[0]}
@@ -97,8 +97,8 @@ export default async function ProductPge({
                 price={data.price}
                 key={data._id}
                 price_id={data.price_id}
-              /> */}
-              <Button variant={"secondary"}>Checkout now</Button>
+              />
+              
             </div>
 
             <p className="mt-12 text-base text-gray-500 tracking-wide">
